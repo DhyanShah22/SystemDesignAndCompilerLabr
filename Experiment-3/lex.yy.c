797766bdabb775bcc4b9500632149bf2f2f81308
DhyanShah22 <dhyanshah229@gmail.com>
2024-08-30 09:07:42 +0530

    Lex Codes

--- Experiment-3/lex.yy.c
+++ Experiment-3/lex.yy.c
@@ -322,6 +322,9 @@ void yyfree ( void *  );
 #define YY_AT_BOL() (YY_CURRENT_BUFFER_LVALUE->yy_at_bol)
 
 /* Begin user sect3 */
+
+#define yywrap() (/*CONSTCOND*/1)
+#define YY_SKIP_YYWRAP
 typedef flex_uint8_t YY_CHAR;
 
 FILE *yyin = NULL, *yyout = NULL;
@@ -351,8 +354,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 5
-#define YY_END_OF_BUFFER 6
+#define YY_NUM_RULES 3
+#define YY_END_OF_BUFFER 4
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -360,20 +363,20 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[9] =
+static const flex_int16_t yy_accept[10] =
     {   0,
-        0,    0,    6,    4,    2,    3,    1,    0
+        0,    0,    4,    2,    3,    2,    0,    1,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    2,    3,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    3,
         1,    4,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -402,24 +405,26 @@ static const YY_CHAR yy_meta[5] =
         1,    1,    1,    1
     } ;
 
-static const flex_int16_t yy_base[9] =
+static const flex_int16_t yy_base[11] =
     {   0,
-        0,    0,    5,    6,    6,    6,    6,    6
+        0,    0,    8,    9,    9,    3,    2,    9,    9,    4
     } ;
 
-static const flex_int16_t yy_def[9] =
+static const flex_int16_t yy_def[11] =
     {   0,
-        8,    1,    8,    8,    8,    8,    8,    0
+        9,    1,    9,    9,    9,   10,   10,    9,    0,    9
     } ;
 
-static const flex_int16_t yy_nxt[11] =
+static const flex_int16_t yy_nxt[14] =
     {   0,
-        4,    5,    6,    7,    8,    3,    8,    8,    8,    8
+        4,    5,    6,    4,    7,    8,    8,    9,    3,    9,
+        9,    9,    9
     } ;
 
-static const flex_int16_t yy_chk[11] =
+static const flex_int16_t yy_chk[14] =
     {   0,
-        1,    1,    1,    1,    3,    8,    8,    8,    8,    8
+        1,    1,    1,    1,   10,    7,    6,    3,    9,    9,
+        9,    9,    9
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -437,12 +442,12 @@ int yy_flex_debug = 0;
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
 #line 1 "prog3.l"
-#line 2 "prog3.l"
-#include <stdio.h>
-
-int spaces = 0, tabs = 0, newlines = 0;
-#line 444 "lex.yy.c"
-#line 445 "lex.yy.c"
+#line 3 "prog3.l"
+    #include <stdio.h>
+    int total_tags;
+    
+#line 449 "lex.yy.c"
+#line 450 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -661,7 +666,7 @@ YY_DECL
 	{
 #line 7 "prog3.l"
 
-#line 664 "lex.yy.c"
+#line 669 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -688,13 +693,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 9 )
+				if ( yy_current_state >= 10 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 6 );
+		while ( yy_base[yy_current_state] != 9 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -719,32 +724,22 @@ do_action:	/* This label is used only to access EOF actions. */
 			goto yy_find_action;
 
 case 1:
+/* rule 1 can match eol */
 YY_RULE_SETUP
 #line 8 "prog3.l"
-{ spaces++; }    
+{total_tags++; printf("HTML tag spotted %s\n",yytext);}
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
 #line 9 "prog3.l"
-{ tabs++; }      
+{}
 	YY_BREAK
 case 3:
-/* rule 3 can match eol */
 YY_RULE_SETUP
 #line 10 "prog3.l"
-{ newlines++; }  
-	YY_BREAK
-case 4:
-YY_RULE_SETUP
-#line 11 "prog3.l"
-{ /* ignore other characters */ }
-	YY_BREAK
-case 5:
-YY_RULE_SETUP
-#line 12 "prog3.l"
 ECHO;
 	YY_BREAK
-#line 747 "lex.yy.c"
+#line 742 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1041,7 +1036,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 9 )
+			if ( yy_current_state >= 10 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1069,11 +1064,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 9 )
+		if ( yy_current_state >= 10 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 8);
+	yy_is_jam = (yy_current_state == 9);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1749,18 +1744,13 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 12 "prog3.l"
+#line 10 "prog3.l"
 
 
-int main(void) {
+int main(){
+    yyin = fopen("sample.html","r");
     yylex();
-    printf("Spaces: %d\n", spaces);
-    printf("Tabs: %d\n", tabs);
-    printf("Newlines: %d\n", newlines);
+    printf("Total HTML tags: %d",total_tags);
+    fclose(yyin);
     return 0;
 }
-
-int yywrap(void) {
-    return 1;
-}
-
