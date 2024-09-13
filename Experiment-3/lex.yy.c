c751eec719f2af1a0aa9084602c9289c5b7be40d
DhyanShah22 <dhyanshah229@gmail.com>
2024-09-13 10:04:06 +0530

    LEXER

--- Experiment-3/lex.yy.c
+++ Experiment-3/lex.yy.c
@@ -354,8 +354,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 3
-#define YY_END_OF_BUFFER 4
+#define YY_NUM_RULES 5
+#define YY_END_OF_BUFFER 6
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -363,9 +363,10 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[10] =
+static const flex_int16_t yy_accept[18] =
     {   0,
-        0,    0,    4,    2,    3,    2,    0,    1,    0
+        0,    0,    6,    3,    4,    3,    0,    1,    0,    0,
+        1,    0,    2,    0,    0,    2,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
@@ -374,9 +375,9 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    3,    1,    1,    1,    1,    4,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    3,
-        1,    4,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -402,29 +403,33 @@ static const YY_CHAR yy_ec[256] =
 
 static const YY_CHAR yy_meta[5] =
     {   0,
-        1,    1,    1,    1
+        1,    2,    1,    1
     } ;
 
-static const flex_int16_t yy_base[11] =
+static const flex_int16_t yy_base[20] =
     {   0,
-        0,    0,    8,    9,    9,    3,    2,    9,    9,    4
+        0,    0,   19,   20,   20,    2,   15,    0,   14,    4,
+        0,    8,   20,    0,   12,    0,   20,   12,   14
     } ;
 
-static const flex_int16_t yy_def[11] =
+static const flex_int16_t yy_def[20] =
     {   0,
-        9,    1,    9,    9,    9,   10,   10,    9,    0,    9
+       17,    1,   17,   17,   17,   17,   18,   19,   18,   18,
+       19,   17,   17,    9,   12,    9,    0,   17,   17
     } ;
 
-static const flex_int16_t yy_nxt[14] =
+static const flex_int16_t yy_nxt[25] =
     {   0,
-        4,    5,    6,    4,    7,    8,    8,    9,    3,    9,
-        9,    9,    9
+        4,    5,    4,    6,    7,    8,   12,   13,   14,   14,
+       15,    9,    9,    9,   11,   16,   10,   10,   17,    3,
+       17,   17,   17,   17
     } ;
 
-static const flex_int16_t yy_chk[14] =
+static const flex_int16_t yy_chk[25] =
     {   0,
-        1,    1,    1,    1,   10,    7,    6,    3,    9,    9,
-        9,    9,    9
+        1,    1,    1,    1,    6,    6,   10,   10,   12,   12,
+       12,   12,   18,   18,   19,   15,    9,    7,    3,   17,
+       17,   17,   17,   17
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -441,13 +446,11 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "prog3.l"
-#line 3 "prog3.l"
-    #include <stdio.h>
-    int total_tags;
-    
-#line 449 "lex.yy.c"
-#line 450 "lex.yy.c"
+#line 1 "prog1.l"
+#line 2 "prog1.l"
+#include <stdio.h>
+#line 452 "lex.yy.c"
+#line 453 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -664,9 +667,10 @@ YY_DECL
 		}
 
 	{
-#line 7 "prog3.l"
+#line 7 "prog1.l"
 
-#line 669 "lex.yy.c"
+
+#line 673 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -693,13 +697,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 10 )
+				if ( yy_current_state >= 18 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 9 );
+		while ( yy_base[yy_current_state] != 20 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -724,22 +728,33 @@ do_action:	/* This label is used only to access EOF actions. */
 			goto yy_find_action;
 
 case 1:
-/* rule 1 can match eol */
 YY_RULE_SETUP
-#line 8 "prog3.l"
-{total_tags++; printf("HTML tag spotted %s\n",yytext);}
+#line 9 "prog1.l"
+; 
 	YY_BREAK
 case 2:
+/* rule 2 can match eol */
 YY_RULE_SETUP
-#line 9 "prog3.l"
-{}
+#line 10 "prog1.l"
+; 
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 10 "prog3.l"
+#line 12 "prog1.l"
+{ fputc(yytext[0], yyout); }
+	YY_BREAK
+case 4:
+/* rule 4 can match eol */
+YY_RULE_SETUP
+#line 13 "prog1.l"
+{ fputc('\n', yyout); }
+	YY_BREAK
+case 5:
+YY_RULE_SETUP
+#line 15 "prog1.l"
 ECHO;
 	YY_BREAK
-#line 742 "lex.yy.c"
+#line 757 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1036,7 +1051,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 10 )
+			if ( yy_current_state >= 18 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1064,11 +1079,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 10 )
+		if ( yy_current_state >= 18 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 9);
+	yy_is_jam = (yy_current_state == 17);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1744,13 +1759,28 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 10 "prog3.l"
+#line 15 "prog1.l"
 
 
-int main(){
-    yyin = fopen("sample.html","r");
-    yylex();
-    printf("Total HTML tags: %d",total_tags);
-    fclose(yyin);
+int main(int argc, char **argv) {
+    if (argc > 2) {
+        FILE *infile = fopen(argv[1], "r");
+        FILE *outfile = fopen(argv[2], "w");
+        if (!infile || !outfile) {
+            printf("Error opening files.\n");
+            return 1;
+        }
+
+        yyin = infile;
+        yyout = outfile;
+
+        yylex();
+
+        fclose(infile);
+        fclose(outfile);
+    } else {
+        printf("Usage: %s <input file> <output file>\n", argv[0]);
+    }
     return 0;
 }
+
